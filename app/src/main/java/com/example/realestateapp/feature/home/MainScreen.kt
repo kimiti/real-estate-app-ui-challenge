f5e386e7f54c589fb8215b14da540f5f1c2bc6c3
kimiti <kimitikevin@gmail.com>
2025-02-06 08:30:00 +0300

    update main screen

--- app/src/main/java/com/example/realestateapp/feature/home/MainScreen.kt
+++ app/src/main/java/com/example/realestateapp/feature/home/MainScreen.kt
@@ -1,2 +1,31 @@
 package com.example.realestateapp.feature.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.realestateapp.R
+import com.example.realestateapp.ui.components.HeaderSection
+
+@Preview
+@Composable
+fun MainScreen() {
+    val items = remember { sampleProperties() }
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .background(colorResource(R.color.lightGrey)),
+        contentPadding = PaddingValues(bottom = 100.dp)
+    ) {
+        item { HeaderSection() }
+    }
+}
