2f8fbfcdce36407dab33d09ee63cd5cf1848cbbd
kimiti <kimitikevin@gmail.com>
2025-02-14 08:30:00 +0300

    updates getDrawable file

--- app/src/main/java/com/example/realestateapp/util/getDrawableId.kt
+++ app/src/main/java/com/example/realestateapp/util/getDrawableId.kt
@@ -1,2 +1,13 @@
 package com.example.realestateapp.util
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun getDrawableId(name: String): Int {
+    val context = LocalContext.current
+    return remember(name) {
+        context.resources.getIdentifier(name, "drawable", context.packageName)
+    }
+}
