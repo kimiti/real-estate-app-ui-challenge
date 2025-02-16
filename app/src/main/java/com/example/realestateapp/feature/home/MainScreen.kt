afba640f89187447e7718a87677aa914fd4b2135
kimiti <kimitikevin@gmail.com>
2025-02-16 11:30:00 +0300

    updates MainScreen Component

--- app/src/main/java/com/example/realestateapp/feature/home/MainScreen.kt
+++ app/src/main/java/com/example/realestateapp/feature/home/MainScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -16,6 +17,7 @@ import androidx.compose.ui.unit.dp
 import com.example.realestateapp.R
 import com.example.realestateapp.ui.components.CategoriesRow
 import com.example.realestateapp.ui.components.HeaderSection
+import com.example.realestateapp.ui.components.PropertyCard
 import com.example.realestateapp.ui.components.SearchRow
 
 @Preview
@@ -35,5 +37,9 @@ fun MainScreen() {
         item { SearchRow() }
         item { Spacer(modifier = Modifier.height(16.dp)) }
         item { CategoriesRow() }
+        items(items){property ->
+            PropertyCard(property)
+        }
+        item { Spacer(modifier = Modifier.height(16.dp)) }
     }
 }
