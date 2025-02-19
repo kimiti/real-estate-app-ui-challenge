1a2f8f700728dad57b12c9d22fe61b3cc7fa499f
kimiti <kimitikevin@gmail.com>
2025-02-19 08:30:00 +0300

    adds bottom navigation bar

--- app/src/main/java/com/example/realestateapp/MainActivity.kt
+++ app/src/main/java/com/example/realestateapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.realestateapp.core.navigation.AppNav
 import com.example.realestateapp.ui.theme.RealEstateAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             RealEstateAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    AppNav()
                 }
             }
         }
