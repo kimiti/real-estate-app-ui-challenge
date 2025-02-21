9dd962d0a324e5f43c8ae23a67087cfd676a22dc
kimiti <kimitikevin@gmail.com>
2025-02-21 08:30:00 +0300

    adds profile screen

--- app/src/main/java/com/example/realestateapp/feature/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/realestateapp/feature/profile/ProfileScreen.kt
@@ -1,4 +1,27 @@
 package com.example.realestateapp.feature.profile
 
-class ProfileScreen {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.realestateapp.R
+import com.example.realestateapp.ui.components.TopBar
+
+@Preview
+@Composable
+fun ProfileScreen(navController: NavController) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(R.color.lightGrey)),
+        contentPadding = PaddingValues(bottom = 100.dp)
+    ) {
+        item { TopBar(onBack = { navController.navigateUp() }) }
+    }
 }
