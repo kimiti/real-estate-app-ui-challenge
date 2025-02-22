2bde654cc5d42a1f36133b162c27693eb87efbc0
kimiti <kimitikevin@gmail.com>
2025-02-22 08:30:00 +0300

    update topBar component

--- app/src/main/java/com/example/realestateapp/ui/components/TopBar.kt
+++ app/src/main/java/com/example/realestateapp/ui/components/TopBar.kt
@@ -13,10 +13,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.realestateapp.R
 
+@Preview
 @Composable
 fun TopBar(onBack: () -> Unit = {}) {
     Box(
