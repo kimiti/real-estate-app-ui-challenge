189382631ba3ea7fd4bf68dcf5d0003b7dd6f967
kimiti <kimitikevin@gmail.com>
2025-02-24 08:30:00 +0300

    update optionRow component

--- app/src/main/java/com/example/realestateapp/ui/components/OptionRow.kt
+++ app/src/main/java/com/example/realestateapp/ui/components/OptionRow.kt
@@ -1,2 +1,57 @@
 package com.example.realestateapp.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.realestateapp.R
+
+@Composable
+fun OptionRow(title: String, onClick: () -> Unit = {}) {
+    val white = colorResource(R.color.white)
+    Row(
+        modifier = Modifier
+            .padding(
+                horizontal = 16.dp,
+                vertical = 8.dp
+            )
+            .fillMaxWidth()
+            .height(60.dp)
+            .clip(RoundedCornerShape(30.dp))
+            .background(white)
+            .clickable { onClick() }
+            .padding(horizontal = 24.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.weight(1f),
+            fontSize = 18.sp,
+            color = colorResource(R.color.black)
+        )
+        Image(
+            painter = painterResource(R.drawable.arrow),
+            contentDescription = null
+        )
+    }
+}
+
+@Preview
+@Composable
+fun OptionRowPreview() {
+    OptionRow(title = "Sample Title")
+}
